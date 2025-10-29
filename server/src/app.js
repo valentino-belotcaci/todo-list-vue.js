8fdd1c22b5dd180fda64d7c1a62fa00c0c8a16ad
Valentino <belotv@usi.ch>
2025-10-29 16:41:11 +0100

    connected mongodb, receives the users that register succesfully

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,44 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const mongoose = require('mongoose')
+const User = require('../models/User')
 
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
 
-app.post('/register', (req, res) => 
-    res.send({
-        message: `Hello ${req.body.email}, your user was registered!`
-    })
-)//get request to a status inpoint
+app.post('/register', async (req, res) => {
+    try {
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password
+        })
 
-app.listen(3000, () => 
-    console.log('Server running on port 3000')
-);
+        await user.save()
+
+        res.send({
+            message: `User ${user.email} registered successfully`
+        })
+    }catch (err) {
+        console.log(err)
+        res.status(400).send({error: 'Email already exists or invalid data'})
+    }
+})//get request to a status inpoint
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => 
+    console.log('Connected to MongoDB')
+  ).catch(err => 
+        console.error('MongoDB connection error:', err)
+    )
+
+// sample route
+app.get('/status', (req, res) => res.send({ message: 'API + MongoDB running' }))
+
+// start server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
